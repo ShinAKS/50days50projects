ca62c62e6a88c6e50cb17aa3aa90caacd3ed55c3
ShinAKS <ayushsinha0@gmail.com>
2021-01-25 23:55:15 +0530

    modified day29

--- d29_double_heart_click/script.js
+++ d29_double_heart_click/script.js
@@ -4,17 +4,8 @@ const times = document.querySelector("#times");
 let clickTime = 0;
 let timesClicked = 0;
 
-loveMe.addEventListener("click", (e) => {
-  if (clickTime === 0) {
-    clickTime = new Date().getTime();
-  } else {
-    if (new Date().getTime() - clickTime < 800) {
-      createHeart(e);
-      clickTime = 0;
-    } else {
-      clickTime = new Date().getTime();
-    }
-  }
+loveMe.addEventListener("dblclick", (e) => {
+  createHeart(e);
 });
 
 const createHeart = (e) => {
